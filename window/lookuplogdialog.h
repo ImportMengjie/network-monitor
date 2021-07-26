91c7a8a75d609102123a75db6f5ea9458a5b575d
ImportMengjie <limengjie@hotmail.com>
2021-07-26 17:38:40 +0800

    refator lookupdialog

--- window/lookuplogdialog.h
+++ window/lookuplogdialog.h
@@ -33,190 +33,28 @@ public:
 
 public slots:
     void handleSearch();
-    void handleNextPage(){
-        if(this->currentPage+1>=getPageCount())
-            return;
-        this->currentPage++;
-        setPageLabel();
-        setTableData();
-    }
-    void handlePrePage(){
-        if(this->currentPage>0){
-            this->currentPage--;
-            setPageLabel();
-            setTableData();
-        }
-    }
-    void handleGoPage(){
-        bool ok;
-        uint page = switchPageEdit->text().toUInt(&ok);
-        if(ok&&page>0){
-            currentPage = page-1;
-            setPageLabel();
-            setTableData();
-        }
-    }
+    void handleNextPage();
+    void handlePrePage();
+    void handleGoPage();
 
-    void saveToCsvFile(){
-        QString fileName = QFileDialog::getSaveFileName(this, "保存日志文件", "", "csv Files (*.csv)");
-        if(fileName.isEmpty()) return;
-        QFile csvFile(fileName);
-        if(csvFile.exists()) return;
-        csvFile.open( QIODevice::ReadWrite | QIODevice::Text );
-        QTextStream out(&csvFile);
-        static QString selectState = "SELECT * FROM running_log";
-        QDateTime start(startDateWidget->date(), QTime(0,0));
-        QDateTime end(endDateWidget->date(), QTime(0,0));
-
-        QString whereState = QString(" where (log_datetime>=:start and log_datetime<=:end)%1%2 order by seq")
-                    .arg(getSelectLogTypeFilter())
-                    .arg(getSelectLogKeyword());
-
-        QString selectWhereState = selectState + whereState;
-        if(!db.isOpen())
-            db.open();
-        if(db.isOpen()){
-            QSqlQuery pageDataQuery(db);
-            pageDataQuery.prepare(selectWhereState);
-            pageDataQuery.bindValue(":start", start);
-            pageDataQuery.bindValue(":end", end);
-            pageDataQuery.exec();
-            if(!pageDataQuery.isActive()){
-                qWarning()<<pageDataQuery.lastError()<<endl;
-                return;
-            }
-            while(pageDataQuery.next()){
-                out<<pageDataQuery.value(0).toUInt()<<','
-                  <<pageDataQuery.value(1).toUInt()<<','
-                 <<pageDataQuery.value(2).toDateTime().toString("yyyy-MM-dd:hh:mm:ss:zzz")<<','
-                <<pageDataQuery.value(3).toString()<<endl;
-            }
-            csvFile.close();
-        }
-
-    }
+    void saveToCsvFile();
 
 private:
 
-    uint getTotalCount(){
-        static QString countState = "SELECT count(*) FROM running_log";
-        QDateTime start(startDateWidget->date(), QTime(0,0));
-        QDateTime end(endDateWidget->date(), QTime(0,0));
+    uint getTotalCount();
 
 
-        QString whereState = QString(" where (log_datetime>=:start and log_datetime<=:end)%1%2 order by seq")
-                    .arg(getSelectLogTypeFilter())
-                    .arg(getSelectLogKeyword());
+    void setTableData();
 
-        QString countWhereState = countState + whereState;
-        if(!db.isOpen())
-            db.open();
-        if(db.isOpen()){
-            QSqlQuery countTotalSize(db);
-            countTotalSize.prepare(countWhereState);
-            countTotalSize.bindValue(":start", start);
-            countTotalSize.bindValue(":end", end);
-            countTotalSize.exec();
-            if(!countTotalSize.isActive()||!countTotalSize.first()){
-                qWarning()<<countTotalSize.lastError()<<endl;
-                return 0;
-            }
-            totalCount = countTotalSize.value(0).toUInt();
-            setPageLabel();
-            return totalCount;
-        }
-        return 0;
-    }
+    uint getPageCount();
 
+    void setPageLabel();
 
-    void setTableData(){
-        static QString selectState = "SELECT * FROM running_log";
-        QDateTime start(startDateWidget->date(), QTime(0,0));
-        QDateTime end(endDateWidget->date(), QTime(0,0));
+    void setTableQuery();
 
-        QString whereState = QString(" where (log_datetime>=:start and log_datetime<=:end)%1%2 order by seq")
-                    .arg(getSelectLogTypeFilter())
-                    .arg(getSelectLogKeyword());
-        QString limitOffsetState = QString(" limit %1 offset %2").arg(pageSize).arg(currentPage*pageSize);
+    QString getSelectLogTypeFilter();
 
-        QString selectWhereState = selectState + whereState + limitOffsetState;
-        if(!db.isOpen())
-            db.open();
-        if(db.isOpen()){
-            QSqlQuery pageDataQuery(db);
-            pageDataQuery.prepare(selectWhereState);
-            pageDataQuery.bindValue(":start", start);
-            pageDataQuery.bindValue(":end", end);
-            pageDataQuery.exec();
-            if(!pageDataQuery.isActive()){
-                qWarning()<<pageDataQuery.lastError()<<endl;
-                return;
-            }
-            tableQuery->setQuery(pageDataQuery);
-            this->setTableQuery();
-
-        }
-        qDebug()<<whereState;
-    }
-
-    uint getPageCount(){
-        return totalCount/pageSize + (totalCount%pageSize==0?0:1);
-    }
-
-    void setPageLabel(){
-        totalPageLabel->setText(QString("共%2条记录 %1页").arg(getPageCount()).arg(totalCount));
-        currentPageLabel->setText(QString("当前%1页").arg(currentPage+1));
-    }
-
-    void setTableQuery(){
-        tableQuery->setHeaderData(running_log::seq, Qt::Horizontal, "序号");
-        tableQuery->setHeaderData(running_log::log_datetime, Qt::Horizontal, "时间");
-        tableQuery->setHeaderData(running_log::log_type, Qt::Horizontal, "类型");
-        tableQuery->setHeaderData(running_log::log_content, Qt::Horizontal, "内容");
-
-        tableView->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-        tableView->setSelectionBehavior(QAbstractItemView::SelectRows);
-        tableView->resizeColumnsToContents();
-        tableView->horizontalHeader()->setStretchLastSection(true);
-
-    }
-
-    QString getSelectLogTypeFilter(){
-        bool status = statusCheckBox->isChecked();
-        bool error = errorCheckBox->isChecked();
-        bool instruct = instructionCheckBox->isChecked();
-        if(!status&&!error&&!instruct)
-            return "";
-        else if(status&&error&&instruct)
-            return "";
-        else{
-            QString start(" and (");
-            QString mid("");
-            mid += status?"log_type=1":"";
-            if(error){
-                if(mid.size())
-                    mid+=" or log_type=2";
-                else
-                    mid+="log_type=2";
-            }
-            if(instruct){
-                if(mid.size())
-                    mid+=" or log_type=3";
-                else
-                    mid+="log_type=3";
-            }
-            return start+mid+")";
-        }
-    }
-
-    QString getSelectLogKeyword(){
-        if(searchKeywordWidget->text().size()){
-            QString text = searchKeywordWidget->text();
-
-            return " and log_content like '%" + text + "%'";
-        }
-        return "";
-    }
+    QString getSelectLogKeyword();
 
     enum running_log{
         seq=0,
